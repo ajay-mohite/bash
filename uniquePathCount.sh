f5d4f8bb78be3f48d92421655a0fe8cfd8c0ee8b
ajay-mohite <ajay.m@browserstack.com>
2025-07-21 15:13:43 +0530

    main setup

--- uniquePathCount.sh
+++
@@ -1,7 +0,0 @@
-# Mix of diff cmds
-# grep "Started" tmp_more_than_50 | awk '{print $3}' | cut -d'?' -f1 | sort | uniq -c | sort -nr
-
-# Just awk cmd more optimal
-awk '/Started/ { split($3, a, "?"); print a[1] }' tmp_more_than_50 | sort | uniq -c | sort -nr
-
-
